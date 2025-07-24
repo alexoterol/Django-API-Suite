7cd1707c2932dc9874095399046d0a96aaefe5ef
Alex Otero <152084340+alexoterol@users.noreply.github.com>
2025-07-24 13:11:12 +0000

    Métodos Put y Patch Corregidos

--- demo_rest_api/urls.py
+++ demo_rest_api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from .views import DemoRestApi, DemoRestApiItem
 from . import views
 
 urlpatterns = [
    path("index/", views.DemoRestApi.as_view(), name="demo_rest_api_resources" ),
-   
+
    # Ruta que recibe un ID como parámetro y usa la vista DemoRestApiItem
-    path("<str:id>/", views.DemoRestApiItem.as_view(), name="demo_rest_api_item"),
+   # ESTA ES LA CLAVE
+   path("<str:id>/", DemoRestApiItem.as_view(), name="demo_rest_api_item"),
 ]
